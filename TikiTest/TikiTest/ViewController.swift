1747ba60a62dd5b3001da23b7ff66a4973b3e06c
Toan <Trungtoan1093@gmail.com>
2018-11-13 09:32:34 +0700

    Commit create project

---
+++ TikiTest/TikiTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TikiTest
+//
+//  Created by Ho Trung Toan on 11/13/18.
+//  Copyright © 2018 Ho Trung Toan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
